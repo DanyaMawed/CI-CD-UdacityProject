d92c0b36f60eedb3b7c7d4a65f1b4fff8a067db4
DanyaMawed <104201638+DanyaMawed@users.noreply.github.com>
2024-04-05 16:54:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
 import logging
+import traceback
 
 import pandas as pd
-import joblib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
+
 
 def scale(payload):
     """Scales Payload"""
@@ -18,23 +20,52 @@ def scale(payload):
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
 
+
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = (
+        "<h3>Sklearn Prediction Home: From Azure Pipelines (Continuous Delivery)</h3>"
+    )
     return html.format(format)
 
+
 # TO DO:  Log out the prediction value
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-    # Performs an sklearn prediction
+    """Performs an sklearn prediction
+
+    input looks like:
+            {
+    "CHAS":{
+      "0":0
+    },
+    "RM":{
+      "0":6.575
+    },
+    "TAX":{
+      "0":296.0
+    },
+    "PTRATIO":{
+       "0":15.3
+    },
+    "B":{
+       "0":396.9
+    },
+    "LSTAT":{
+       "0":4.98
+    }
+
+    result looks like:
+    { "prediction": [ 20.35373177134412 ] }
+
+    """
+
     try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+        clf = joblib.load("boston_housing_prediction.joblib")
+    except Exception as e:
+        LOG.error("Error loading model: %s", str(e))
+        LOG.error("Exception traceback: %s", traceback.format_exc())
+    return "Model not loaded"
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
@@ -42,7 +73,8 @@ def predict():
     LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
-    return jsonify({'prediction': prediction})
+    return jsonify({"prediction": prediction})
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
